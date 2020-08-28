26b8cfc1420dcc672bc32c7186d045e5bb8af9ef
A Suicidal Cow <aaron.bertagnole@gmail.com>
2020-08-27 22:01:59 -0600

    Added navbar to home will update to component  and added portfolio page

---
+++ src/pages/Portfolio.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Portfolio extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <div className='card'>
+          <a href='https://github.com/ASuicidalCow'></a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Portfolio;
